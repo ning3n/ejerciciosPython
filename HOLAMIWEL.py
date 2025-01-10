fb7c8746efd149d7861132eec87b8a31454dfb74
Jesus Miguel Rodriguez <52975938+ning3n@users.noreply.github.com>
2025-01-10 18:41:13 -0400

    Add files via upload

---
+++ HOLAMIWEL.py
@@ -0,0 +1,5 @@
+#Hola mundo! Me llamo miwel y voy a estar practicando full en este 2025 programacion
+# https://github.com/mouredev/Hello-Python?tab=readme-ov-file
+# Voy a imprimir mi primer hola mundo desde mi regreso, A APRENDER!
+
+print("Hola Mundo!")
