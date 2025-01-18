169e2cd7590f3ea4cdfb6dbd5fe653262cbb4cd7
Jesus Miguel Rodriguez <52975938+ning3n@users.noreply.github.com>
2025-01-17 20:27:43 -0400

    Add files via upload

---
+++ aMayuscula.js
@@ -0,0 +1,7 @@
+//Programa que recibe una cadena de texto y retorna la primera letra en mayuscula
+
+function aMayuscula(cadena) {
+    return cadena.charAt(0).toUpperCase();
+}
+
+console.log(aMayuscula("hola")); // H
