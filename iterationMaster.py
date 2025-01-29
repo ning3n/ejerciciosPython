b6013c50a9def7514b21f37ae38c01b5e3699b96
Jesus Miguel Rodriguez <52975938+ning3n@users.noreply.github.com>
2025-01-28 21:14:28 -0400

    Add files via upload

---
+++ iterationMaster.py
@@ -0,0 +1,19 @@
+numeros = list(range(1,101))
+for numero in numeros100:
+    print(numero)
+
+print('', end='\n')
+
+for num in range(1,101):
+    print(num)
+
+print('', end='\n')
+
+num = 0
+
+while num < 100:
+    num += 1
+    print(num)
+
+print('', end='\n')
+
